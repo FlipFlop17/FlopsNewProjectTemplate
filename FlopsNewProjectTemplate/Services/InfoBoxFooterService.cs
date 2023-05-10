0abbf10035772a74c3eede46fe1f47ca6b6eff96
FlipFlop17 <42313405+FlipFlop17@users.noreply.github.com>
2023-05-10 22:23:25 +0200

    adding footer msg info placeholder

---
+++ FlopsNewProjectTemplate/Services/InfoBoxFooterService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlopsNewProjectTemplate.Services
+{
+    public enum FooterMsgType
+    {
+        Positive,
+        Error,
+        Neutral,
+        Warning
+    }
+    public class InfoBoxFooterService
+    {
+        public string MessageToShow { get; set; }
+
+    }
+}
